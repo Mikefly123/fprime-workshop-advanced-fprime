7796ea3913af1c3a58feadcb1b23b669b238d3c1
Zimri Leisher <zimri@umich.edu>
2026-02-12 19:04:44 -0800

    added a bunch of states

--- imuStates/Main.cpp
+++ imuStates/Main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char* argv[]) {
 
     // Setup, cycle, and teardown topology
     imuStates::setupTopology(inputs);
-    imuStates::startRateGroups(Fw::TimeInterval(1,0));  // Program loop cycling rate groups at 1Hz
+    imuStates::startRateGroups(Fw::TimeInterval(0,10000));  // Program loop cycling rate groups at 1Hz
     imuStates::teardownTopology(inputs);
     (void)printf("Exiting...\n");
     return 0;
